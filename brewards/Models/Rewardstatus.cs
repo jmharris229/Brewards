34c712c4d139e66ba0d6342d295d7e9a64687283
jmharris229 <joelharris229@gmail.com>
2016-05-17 16:38:56 -0500

    added rewardstatus and userpurchase classes

---
+++ brewards/Models/Rewardstatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace brewards.Models
+{
+    public class Rewardstatus
+    {
+        public int RewardstatusId { get; set; }
+        public int BreweryId { get; set; }
+        public int UserId { get; set; }
+        public int Number_purchased { get; set; }
+    }
+}
---
+++ brewards/Models/Userpurchases.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace brewards.Models
+{
+    public class Userpurchases
+    {
+        public int UserpurchaseId { get; set; }
+        public int UserId { get; set; }
+        public int BeerId { get; set; }
+        public DateTime Purchase_date { get; set; }
+    }
+}
