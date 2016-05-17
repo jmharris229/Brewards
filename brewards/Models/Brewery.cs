524d8e81f32e0dfd1583d078cd51ba5d72f7eb3e
jmharris229 <joelharris229@gmail.com>
2016-05-17 16:34:31 -0500

    added brewery class

---
+++ brewards/Models/Brewery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace brewards.Models
+{
+    public class Brewery
+    {
+        public int BreweryId { get; set; }
+        public int Brewery_longitude { get; set; }
+        public int Brewery_latitude { get; set; }
+        public string Brewery_phone { get; set; }
+        public string Brewery_url { get; set; }
+        public string Brewery_logo { get; set; }
+    }
+}
