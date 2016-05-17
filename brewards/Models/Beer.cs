16b23a3016f805b2f20a9c0d83fc0d2c21e0707d
jmharris229 <joelharris229@gmail.com>
2016-05-17 16:31:27 -0500

    added beer class

---
+++ brewards/Models/Beer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace brewards.Models
+{
+    public class Beer
+    {
+        public int BeerId { get; set; }
+        public int BreweryId { get; set; }
+        public string Beer_name { get; set; }
+        public string Beer_type { get; set; }
+        public string Beer_logo { get; set; }
+    }
+}
