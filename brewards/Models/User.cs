64f3f867b8f7110ffacde81dc93c8e4ec76fc2d1
jmharris229 <joelharris229@gmail.com>
2016-05-18 11:10:15 -0500

    made minor changes, added user class

---
+++ brewards/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace brewards.Models
+{
+    public class User
+    {
+        //retrieves user id and sets as primary key
+        public virtual ApplicationUser UserId { get; set; }
+        public virtual ICollection<Userpurchase> User_purchases { get; set; }
+    }
+}
