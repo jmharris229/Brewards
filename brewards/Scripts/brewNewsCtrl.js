d15746dc3ef96d6afc61ccff499ced8d6700c2d2
Joel Harris <joelharris229@gmail.com>
2016-06-17 21:55:49 -0500

    styled brewery news section

--- brewards/Scripts/brewNewsCtrl.js
+++ brewards/Scripts/brewNewsCtrl.js
@@ -1,10 +1,19 @@
-﻿app.controller('brewNewsCtrl', function($http){
+﻿app.controller('brewNewsCtrl', function($http, locService){
     var vm = this;
 
-    vm.test = "hello";
     vm.offers;
+
+    //retrieves offers specific to city, switch out city for locService city once fully implemented for test this is fine. 
     $http.get('api/BreweryNews?breweryCity=Nashville')
         .then(function (response) {
-            vm.offers = response.data;
+
+            //process to change date to more readible format
+            var editedOffers = response.data;
+            for (var i = 0; i < editedOffers.length; i++) {
+                var tIndex = editedOffers[i].newsDate.indexOf('T');
+                var slicedDate = editedOffers[i].newsDate.substring(0, tIndex);
+                editedOffers[i].newsDate = new Date(slicedDate).toDateString();
+            }
+            vm.offers = editedOffers;
         });
 })
